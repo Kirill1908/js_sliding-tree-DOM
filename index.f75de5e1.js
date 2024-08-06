129f645cf4930c3198ce722030ceeeaadc4b4f65
Kirill Gasan <dp160190gkj@gmail.com>
2024-08-06 21:53:17 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,17 @@
+"use strict";
+const tree = document.querySelector(".tree");
+const liElements = tree.querySelectorAll("li");
+for (const item of liElements){
+    const spanWithTitle = document.createElement("span");
+    item.prepend(spanWithTitle);
+    spanWithTitle.append(spanWithTitle.nextSibling);
+}
+tree.addEventListener("click", function(e) {
+    if (e.target.tagName !== "SPAN") return;
+    const container = e.target.parentNode.querySelector("ul");
+    if (!container) return;
+    if (!container.hidden) container.hidden = true;
+    else container.hidden = false;
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
